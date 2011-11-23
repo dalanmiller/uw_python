2f60b7d0211964ae7efd53de1f6db6c8e20e0846
Daniel Miller <dalan.miller@gmail.com>
2011-11-22 18:51:20 -0800

    atom.py finally complete, why can't I visualize iterations sometimes?

--- exercise6/atom.py
+++ exercise6/atom.py
@@ -5,7 +5,7 @@ Usage:
 Call form main namespace to run test code
 
 Notes:
-As of this commit, the only thing not working is limiting the bonding to the MAX_BONDS variable
+As of this commit, everything works! Why I was previously using <= I have no idea...
 
 
 #def atomCheck(self, other):
@@ -21,7 +21,8 @@ import sys, inspect
 
 
 class Atom(object):
-    """This is a class outlining the attributes of an atom"""
+    MAX_BONDS = 0
+    """This is a class outlining & intializing the attributes of an atom"""
     def __init__(self, symbol, bonds, number):
         
         if symbol: 
@@ -47,68 +48,54 @@ class Atom(object):
         if self.bonds.__len__() == 0:
             return "Atomic Number: %s\nSymbol: %s" % (self.atomic_number, self.chemical_symbol)
         else:
-            
-            """Found out it was faster to just have a list of objects versus id -> find object based off id."""
-            #bonded = []
-            #for name, obj in inspect.getmembers(sys.modules[__name__]):
-            #    if isinstance(obj, Atom) and obj.id in self.bonds:
-            #        bonded.append({'name':name,'atomic':obj.atomic_number,'symbol':obj.chemical_symbol,'id':obj.id})
-
-            return "Atomic Number: %s\nSymbol: %s\nand is bonded to the following atom(s):\n%s" % (self.atomic_number, self.chemical_symbol, [x for x in self.bonds])
+            s = ""
+            for x in self.bonds:
+                s += "Atomic Number: %s - Name: %s -  Id: %s\n" % (x.atomic_number,x.__class__.__name__, id(x))
+            return u"Atomic Number: %s\nSymbol: %s\nand is bonded to the following atom(s):\n%s" % (self.atomic_number, self.chemical_symbol, s)
 
     def __add__(self, other_atom):
-        if isinstance(other_atom, Atom) and self.id != other_atom.id:
+        """Overwriting builtin for adding Atoms together, adds the other object of Atom to each bond list"""
+        if (isinstance(other_atom, Atom) and self.id != other_atom.id and self.num_bonds() < self.MAX_BONDS and other_atom.num_bonds() < other_atom.MAX_BONDS): #Checks that the given object is an instance of Atom, that the given object isn't the same Atom is "self" Atom, and that both Atoms have an available space for a bond.
             self.bonds.append(other_atom)
             other_atom.bonds.append(self)
-        else:
-            print "Type Error! Uh oh! (Or perhaps another unforeseen error)"
+            return other_atom
 
     def __cmp__(self, other_atom):
         """Checks atoms based on atomic number"""
         if isinstance(other_atom, Atom):
             return self.atomic_number == other_atom.atomic_number
-            
-
+           
     def get_symbol(self):
         """returns string symbol"""
         return self.chemical_symbol
 
     def get_bonds(self):
+        """Returns bond list"""
         return self.bonds
 
     def get_number(self):
+        """Returns atomic number"""
         return self.atomic_number
 
     def get_protons(self):
+        """Returns number of protons / atomic number"""
         return get_number(self)
 
     def num_bonds(self):
+        """Returns number of bonds currently in place"""
         return self.bonds.__len__()
 
 class Oxygen(Atom):
     """Oxygen sublcass of Atom superclass"""
     MAX_BONDS = 2
     def __init__(self):
-        Atom.__init__(self, "O", [], 8)
-    
-    def __add__(self,other_atom):
-        if self.num_bonds() <= self.MAX_BONDS and other_atom.num_bonds() <= other_atom.MAX_BONDS:
-            Atom.__add__(self,other_atom)            
-        else:
-            print "Captain! We can't bond anymore!"         
+        Atom.__init__(self, "O", [], 8)      
 
 class Hydrogen(Atom):
     """Hydrogen subclass of Atom superclass"""
     MAX_BONDS = 1
     def __init__(self):
         Atom.__init__(self, "H", [], 1)
-        
-    def __add__(self,other_atom):
-        if self.num_bonds() <= self.MAX_BONDS and other_atom.num_bonds() <= other_atom.MAX_BONDS:
-            Atom.__add__(self,other_atom)            
-        else:
-            print "Captain! We can't bond anymore!"
-
 
 if __name__ == "__main__":
     """Test code begins here"""
